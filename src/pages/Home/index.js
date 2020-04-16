a40782667fcc6e447deecafc5a420c1922a4face
valls <thiago@agenciatw.com.br>
2020-04-16 00:29:55 -0300

    creating header component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,10 +3,5 @@ import React from 'react';
 import { Title } from './styles';
 
 export default function Home() {
-  return (
-      <Title>
-        Home
-        <small> page</small>
-      </Title>
-    );
+  return <div />;
 }
