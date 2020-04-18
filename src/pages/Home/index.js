0292272eb0ecd16850163b51867fa3fb59be8b9a
valls <thiago@agenciatw.com.br>
2020-04-17 23:31:31 -0300

    render product list from fake api

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,71 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
+
+import api from './../../services/api';
+import { formatPrice } from './../../util/format';
 
 import { ProductList } from './styles';
 
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-mizuno-wave-eagle-2-masculino/20/D16-4893-120/D16-4893-120_zoom1.jpg?ts=1570451717&ims=326x"
-          alt="Tenis Nike"
-        />
-        <strong>tÊNIS MUITO LEGAL</strong>
-        <span>R$129,00</span>
+export default class Home extends Component {
+  state = {
+    products: [],
+  };
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-mizuno-wave-eagle-2-masculino/20/D16-4893-120/D16-4893-120_zoom1.jpg?ts=1570451717&ims=326x"
-          alt="Tenis Nike"
-        />
-        <strong>tÊNIS MUITO LEGAL</strong>
-        <span>R$129,00</span>
+  async componentDidMount() {
+    const response = await api.get('products');
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-mizuno-wave-eagle-2-masculino/20/D16-4893-120/D16-4893-120_zoom1.jpg?ts=1570451717&ims=326x"
-          alt="Tenis Nike"
-        />
-        <strong>tÊNIS MUITO LEGAL</strong>
-        <span>R$129,00</span>
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price)
+    }));
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-mizuno-wave-eagle-2-masculino/20/D16-4893-120/D16-4893-120_zoom1.jpg?ts=1570451717&ims=326x"
-          alt="Tenis Nike"
-        />
-        <strong>tÊNIS MUITO LEGAL</strong>
-        <span>R$129,00</span>
+    this.setState({
+      products: data
+    });
+  }
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-    </ProductList>
-  );
+  render() {
+    const { products } = this.state;
+
+    return (
+      <ProductList>
+        {
+          products.map(product => (
+            <li key={product.id}>
+              <img
+                src={product.image}
+                alt={product.title}
+              />
+              <strong>{product.title}</strong>
+              <span>{product.priceFormatted}</span>
+
+              <button type="button">
+                <div>
+                  <MdAddShoppingCart size={16} color="#fff" /> 1
+            </div>
+                <span>ADICIONAR AO CARRINHO</span>
+              </button>
+            </li>
+          ))
+
+        }
+      </ProductList>
+    );
+  }
 }
