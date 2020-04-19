de58aacdbe40525f1190293e75770c24121be724
valls <thiago@agenciatw.com.br>
2020-04-19 13:20:06 -0300

    Block action with duplicity product on cart, adding 1 in amount instead

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {
   MdAddCircleOutline,
   MdRemoveCircleOutline,
@@ -8,67 +8,64 @@ import { connect } from 'react-redux';
 
 import { Container, ProductTable, Total } from './styles';
 
-class Cart extends Component {
-  render() {
-    const { items, total } = this.props;
-
-    return (
-      <Container>
-        <ProductTable>
-          <thead>
-            <tr>
-              <th />
-              <th>Product</th>
-              <th>QTD</th>
-              <th>SUBTOTAL</th>
-              <th />
-            </tr>
-          </thead>
-          <tbody>
-            {items.map((item) => (
-              <tr key={item.id}>
-                <td>
-                  <img src={item.image} alt={item.title} />
-                </td>
-                <td>
-                  <strong>{item.title}</strong>
-                  <span>{item.price}</span>
-                </td>
-                <td>
-                  <div>
-                    <button type="button">
-                      <MdRemoveCircleOutline size={20} color="#7159c1" />
-                    </button>
-                    <input type="number" readOnly value={1} />
-                    <button type="button">
-                      <MdAddCircleOutline size={20} color="#7159c1" />
-                    </button>
-                  </div>
-                </td>
-                <td>{1 * item.price}</td>
-                <td>
+function Cart({ cart }) {
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>Product</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          {cart.map((product) => (
+            <tr key={product.id}>
+              <td>
+                <img src={product.image} alt={product.title} />
+              </td>
+              <td>
+                <strong>{product.title}</strong>
+                <span>{product.priceFormatted}</span>
+              </td>
+              <td>
+                <div>
                   <button type="button">
-                    <MdDelete size={20} color="#7159c1" />
+                    <MdRemoveCircleOutline size={20} color="#7159c1" />
                   </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </ProductTable>
+                  <input type="number" readOnly value={product.amount} />
+                  <button type="button">
+                    <MdAddCircleOutline size={20} color="#7159c1" />
+                  </button>
+                </div>
+              </td>
+              <td>R$155,00</td>
+              <td>
+                <button type="button">
+                  <MdDelete size={20} color="#7159c1" />
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </ProductTable>
 
-        <footer>
-          <button type="button">Finalizar pedido</button>
-          <Total>
-            <span>TOTAL</span>
-            <strong>{total}</strong>
-          </Total>
-        </footer>
-      </Container>
-    );
-  }
+      <footer>
+        <button type="button">Finalizar pedido</button>
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$1920,00</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
 }
 
-export default connect((state) => ({
-  items: state.cart,
-  total: 1000,
-}))(Cart);
+const mapStateToProps = (state) => ({
+  cart: state.cart,
+});
+
+export default connect(mapStateToProps)(Cart);
